995207a7fed9654213a481082cc5be013412111d
sundar <sundar.s@applab.qa>
2024-08-14 17:25:49 +0530

    second commit

--- app/src/main/java/com/example/githubdemo/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-       //first commit
+       //second commit done
 
     }
 }
